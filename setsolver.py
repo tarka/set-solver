16fc15c2c0e41a04fa913a761ed30ab710056558
Steve Smith <tarkasteve@gmail.com>
2009-09-18 22:35:58 +1000

    Add threshold filtering

--- setsolver.py
+++ setsolver.py
@@ -1,12 +1,22 @@
 
 from PIL import Image
-from colorsys import rgb_to_hsv, hsv_to_rgb 
+from colorsys import rgb_to_hsv, hsv_to_rgb
+from functools import partial
 
 def pxToFloat(px):
     return (float(px[0])/255,float(px[1])/255,float(px[2])/255)
 
 def pxToInt(px):
     return (int(px[0]*255), int(px[1]*255), int(px[2]*255))
+
+# Apply a function to every pixel, returning a new image/pixaccess pair
+def filter(img, fn):
+    new = img.copy()
+    size = new.size
+    for x in range(size[0]):
+        for y in range(size[1]):
+            new.putpixel((x,y), fn(new.getpixel((x,y))))
+    return new
 
 # Convert to greyscale by removing saturation and hue, and averaging
 # RGB to be V
@@ -15,23 +25,22 @@ def toGreyscale(px):
     rgb = hsv_to_rgb(0,0, reduce(lambda a,b: a+b, fpx)/3.0)
     return pxToInt(rgb)
 
+# All px above val == white, all below == black
+def threshold(th, px):
+    v = rgb_to_hsv(*pxToFloat(px))[2]
+    if v < th:
+        return (0,0,0)
+    else:
+        return (255,255,255)
+
+
 class SetImage(object):
     
     def __init__(self, giffile):
         img = Image.open(giffile)
         self.img = img.convert()     # RGB by default
-        self.pix = self.img.load()        # Pixel-array representation
-        self.gsimg, self.gspix = self.filter(toGreyscale)
-
+        self.gsimg = filter(self.img, toGreyscale)
+        self.thimg = filter(self.gsimg, partial(threshold, 0.6))
     
-    # Apply a function to every pixel, returning a new image/pixaccess pair
-    def filter(self, fn):
-        new = self.img.copy()
-        npx = new.load()
-        size = new.size
-        for x in range(size[0]):
-            for y in range(size[1]):
-                npx[x,y]=fn(npx[x,y])
-        return (new,npx)
 
     
--- test.py
+++ test.py
@@ -3,4 +3,5 @@ from setsolver import SetImage
 
 si = SetImage("test/01.gif")
 #si.img.show()
-si.gsimg.show()
+#si.gsimg.show()
+si.thimg.show()
