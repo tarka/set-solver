56787b757973429e3918eef0287b905cc24e9e68
Steve Smith <tarkasteve@gmail.com>
2009-09-19 10:03:31 +1000

    Add linecount and flood-fill routines

--- setsolver.py
+++ setsolver.py
@@ -1,7 +1,10 @@
 
+import sys
 from PIL import Image
 from colorsys import rgb_to_hsv, hsv_to_rgb
 from functools import partial
+
+sys.setrecursionlimit(10000)
 
 def pxToFloat(px):
     return (float(px[0])/255,float(px[1])/255,float(px[2])/255)
@@ -9,8 +12,8 @@ def pxToFloat(px):
 def pxToInt(px):
     return (int(px[0]*255), int(px[1]*255), int(px[2]*255))
 
-# Apply a function to every pixel, returning a new image/pixaccess pair
-def filter(img, fn):
+# Return a new image which has had fn applied to each pixel
+def pxfilter(img, fn):
     new = img.copy()
     size = new.size
     for x in range(size[0]):
@@ -33,14 +36,36 @@ def threshold(th, px):
     else:
         return (255,255,255)
 
+# All pixels not of color => white
+def tocolor(rgb, px):
+    if px != rgb:
+        return (255,255,255)
+    else:
+        return px
+
+ # 1. If the color of node is not equal to target-color, return.
+ # 2. If the color of node is equal to replacement-color, return.
+ # 3. Set the color of node to replacement-color.
+ # 4. Perform Flood-fill (west of node, target-color, replacement-color).
+ #    Perform Flood-fill (east of node, target-color, replacement-color).
+ #    Perform Flood-fill (north of node, target-color, replacement-color).
+ #    Perform Flood-fill (south of node, target-color, replacement-color).
+ # 5. Return.
+def flood(img, target, replacement, sx, sy, x=0, y=0):
+    if img[x,y] != target: return
+    if img[x,y] == replacement: return
+    img[x,y] = replacement
+    if x-1 >= 0: flood(img, target, replacement, sx, sy, x-1, y)
+    if x+1 < sx: flood(img, target, replacement, sx, sy, x+1, y)
+    if y-1 >= 0: flood(img, target, replacement, sx, sy, x, y-1)
+    if y+1 < sy: flood(img, target, replacement, sx, sy, x, y+1)
 
 class SetImage(object):
     
     def __init__(self, giffile):
         img = Image.open(giffile)
         self.img = img.convert()     # RGB by default
-        self.gsimg = filter(self.img, toGreyscale)
-        self.thimg = filter(self.gsimg, partial(threshold, 0.6))
+        self.bwimg = pxfilter(pxfilter(self.img, toGreyscale), partial(threshold, 0.6))
 
     # FIXME: Make SetImage iterable?
     def color(self):
@@ -58,6 +83,21 @@ class SetImage(object):
         getter = lambda a:a[1]
         return sorted(d.iteritems(), key=getter, reverse=True)[0][0] 
 
-            
+    def linecount(self):
+        trans = 0
+        size = self.bwimg.size
+        for x in range(size[0]):
+            prev = self.bwimg.getpixel((x,0))
+            ttrans = 0
+            for y in range(size[1]):
+                p = self.bwimg.getpixel((x,y))
+                if prev != p:
+                    print x,prev,p
+                    ttrans += 1
+                    prev = p
 
-    
+            if ttrans > trans:
+                trans = ttrans
+
+        return trans
+
--- test.py
+++ test.py
@@ -1,8 +1,19 @@
 
+import sys
+import setsolver as ss
 from setsolver import SetImage
 
-si = SetImage("test/33.gif")
+
+
+si = SetImage("test/03.gif")
 #si.img.show()
 #si.gsimg.show()
-#si.thimg.show()
-print si.color()
+#si.bwimg.show()
+#print si.color()
+#print si.linecount()
+
+s2 = si.bwimg.copy()
+#sx,sy = s2.size
+ss.flood(s2.load(), (255,255,255), (255,0,0), *s2.size)
+s2.show()
+
