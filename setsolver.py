27591240e31fac2bc5e86d58c06a517c5c23a1c1
Steve Smith <tarkasteve@gmail.com>
2009-09-19 10:52:27 +1000

    Add count operation

--- setsolver.py
+++ setsolver.py
@@ -6,11 +6,13 @@ from functools import partial
 
 sys.setrecursionlimit(10000)
 
+
 def pxToFloat(px):
     return (float(px[0])/255,float(px[1])/255,float(px[2])/255)
 
 def pxToInt(px):
     return (int(px[0]*255), int(px[1]*255), int(px[2]*255))
+
 
 # Return a new image which has had fn applied to each pixel
 def pxfilter(img, fn):
@@ -21,12 +23,14 @@ def pxfilter(img, fn):
             new.putpixel((x,y), fn(new.getpixel((x,y))))
     return new
 
+
 # Convert to greyscale by removing saturation and hue, and averaging
 # RGB to be V
 def toGreyscale(px):
     fpx = pxToFloat(px)
     rgb = hsv_to_rgb(0,0, reduce(lambda a,b: a+b, fpx)/3.0)
     return pxToInt(rgb)
+
 
 # All px above val == white, all below == black
 def threshold(th, px):
@@ -36,29 +40,39 @@ def threshold(th, px):
     else:
         return (255,255,255)
 
-# All pixels not of color => white
-def tocolor(rgb, px):
+
+# All pixels not of colour => white
+def tocolour(rgb, px):
     if px != rgb:
         return (255,255,255)
     else:
         return px
 
- # 1. If the color of node is not equal to target-color, return.
- # 2. If the color of node is equal to replacement-color, return.
- # 3. Set the color of node to replacement-color.
- # 4. Perform Flood-fill (west of node, target-color, replacement-color).
- #    Perform Flood-fill (east of node, target-color, replacement-color).
- #    Perform Flood-fill (north of node, target-color, replacement-color).
- #    Perform Flood-fill (south of node, target-color, replacement-color).
- # 5. Return.
-def flood(img, target, replacement, sx, sy, x=0, y=0):
-    if img[x,y] != target: return
-    if img[x,y] == replacement: return
-    img[x,y] = replacement
-    if x-1 >= 0: flood(img, target, replacement, sx, sy, x-1, y)
-    if x+1 < sx: flood(img, target, replacement, sx, sy, x+1, y)
-    if y-1 >= 0: flood(img, target, replacement, sx, sy, x, y-1)
-    if y+1 < sy: flood(img, target, replacement, sx, sy, x, y+1)
+
+# Return new copy of image that has been flooded
+def flooded(img, target, replacement):    
+    i2 = img.copy()
+
+    # 1. If the colour of node is not equal to target-colour, return.
+    # 2. If the colour of node is equal to replacement-colour, return.
+    # 3. Set the colour of node to replacement-colour.
+    # 4. Perform Flood-fill (west of node, target-colour, replacement-colour).
+    #    Perform Flood-fill (east of node, target-colour, replacement-colour).
+    #    Perform Flood-fill (north of node, target-colour, replacement-colour).
+    #    Perform Flood-fill (south of node, target-colour, replacement-colour).
+    # 5. Return.
+    def flood(img, target, replacement, sx, sy, x=0, y=0):
+        if img[x,y] != target: return
+        if img[x,y] == replacement: return
+        img[x,y] = replacement
+        if x-1 >= 0: flood(img, target, replacement, sx, sy, x-1, y)
+        if x+1 < sx: flood(img, target, replacement, sx, sy, x+1, y)
+        if y-1 >= 0: flood(img, target, replacement, sx, sy, x, y-1)
+        if y+1 < sy: flood(img, target, replacement, sx, sy, x, y+1)
+
+    flood(i2.load(), target, replacement, *i2.size)
+    return i2
+
 
 class SetImage(object):
     
@@ -66,9 +80,16 @@ class SetImage(object):
         img = Image.open(giffile)
         self.img = img.convert()     # RGB by default
         self.bwimg = pxfilter(pxfilter(self.img, toGreyscale), partial(threshold, 0.6))
+        self._colour = None
+        self._count = None
+        self._pattern = None
+        self._shape = None
 
     # FIXME: Make SetImage iterable?
-    def color(self):
+    @property
+    def colour(self):
+        if self._colour != None: return self._colour
+
         # Count colour occurences, ignoring white. Return most common.
         d={}
         size = self.img.size
@@ -81,8 +102,32 @@ class SetImage(object):
                     else:
                         d[p] = 1
         getter = lambda a:a[1]
-        return sorted(d.iteritems(), key=getter, reverse=True)[0][0] 
+        
+        self._colour = sorted(d.iteritems(), key=getter, reverse=True)[0][0] 
+        return self._colour
 
+    @property
+    def count(self):
+        if self._count != None: return self._count
+
+        # To count the shapes we flood-fill the non-shape parts of the
+        # BW version of the image with a known color.  We then
+        # ray-cast through the middle of the image and count the
+        # transitions:
+        fcol = (255,0,0)
+        fimg = flooded(self.bwimg, (255,255,255), fcol)
+        y = fimg.size[1]/2
+        trans = 0
+        prev = fcol
+        for x in range(fimg.size[0]):
+            p = fimg.getpixel((x,y))
+            if p != fcol and prev == fcol:
+                # This is a rising edge; count
+                trans += 1
+            prev = p
+        self._count = trans
+        return self._count
+    
     def linecount(self):
         trans = 0
         size = self.bwimg.size
--- test.py
+++ test.py
@@ -1,19 +1,44 @@
 
-import sys
+import sys, logging, unittest
 import setsolver as ss
 from setsolver import SetImage
 
 
 
-si = SetImage("test/03.gif")
-#si.img.show()
-#si.gsimg.show()
-#si.bwimg.show()
-#print si.color()
-#print si.linecount()
 
-s2 = si.bwimg.copy()
-#sx,sy = s2.size
-ss.flood(s2.load(), (255,255,255), (255,0,0), *s2.size)
-s2.show()
+class TestCount(unittest.TestCase):
 
+    def testFilledSquiggleCount(self):
+        self.assertEqual(SetImage("test/01.gif").count, 1)
+        self.assertEqual(SetImage("test/02.gif").count, 2)
+        self.assertEqual(SetImage("test/03.gif").count, 3)
+
+    def testFilledDiamondCount(self):
+        self.assertEqual(SetImage("test/10.gif").count, 1)
+        self.assertEqual(SetImage("test/11.gif").count, 2)
+        self.assertEqual(SetImage("test/12.gif").count, 3)
+
+    def testFilledOvalCount(self):
+        self.assertEqual(SetImage("test/19.gif").count, 1)
+        self.assertEqual(SetImage("test/20.gif").count, 2)
+        self.assertEqual(SetImage("test/21.gif").count, 3)
+
+    def testEmptySquiggleCount(self):
+        self.assertEqual(SetImage("test/55.gif").count, 1)
+        self.assertEqual(SetImage("test/56.gif").count, 2)
+        self.assertEqual(SetImage("test/57.gif").count, 3)
+
+    def testEmptyDiamondCount(self):
+        self.assertEqual(SetImage("test/67.gif").count, 1)
+        self.assertEqual(SetImage("test/68.gif").count, 2)
+        self.assertEqual(SetImage("test/69.gif").count, 3)
+
+    def testEmptyOvalCount(self):
+        self.assertEqual(SetImage("test/79.gif").count, 1)
+        self.assertEqual(SetImage("test/80.gif").count, 2)
+        self.assertEqual(SetImage("test/81.gif").count, 3)
+
+
+if __name__ == '__main__':
+    #logging.root.setLevel(logging.DEBUG)
+    unittest.main()
